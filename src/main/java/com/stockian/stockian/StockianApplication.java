9ce2dbd2224d0c73fa05ed7b51531386b1ecc01c
restar <restar.milan.balint@otpmobil.com>
2022-08-07 15:47:55 +0200

    initial commit

---
+++ src/main/java/com/stockian/stockian/StockianApplication.java
@@ -0,0 +1,13 @@
+package com.stockian.stockian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockianApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StockianApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/stockian/stockian/StockianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockian.stockian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockianApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
