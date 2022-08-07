5a3292da3c074863c583e573b2e3fb37a8b380dd
restar <restar.milan.balint@otpmobil.com>
2022-08-07 16:19:28 +0200

    add exclude to spring boot application starter

--- src/main/java/com/stockian/stockian/StockianApplication.java
+++ src/main/java/com/stockian/stockian/StockianApplication.java
@@ -2,8 +2,9 @@ package com.stockian.stockian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class StockianApplication {
 
   public static void main(String[] args) {
